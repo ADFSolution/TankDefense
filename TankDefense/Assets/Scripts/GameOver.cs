adfa1ef110880920dc00a6daa6ec1bb89fa49c3a
ADF Games <fonseca.deivid@gmail.com>
2015-06-08 21:21:18 -0300

    subida de splash, menu start e game over,

---
+++ TankDefense/Assets/Scripts/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    private Gerenciador gerenciador;
+    private GUISkin SKIN;
+
+
+	// Use this for initialization
+	void Start () {
+        gerenciador = FindObjectOfType(typeof(Gerenciador)) as Gerenciador;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = SKIN;
+
+
+        if (GUI.Button(new Rect(Screen.width - 750, 350, 150, 50), "Start"))
+        {
+            gerenciador.ProximoLevel(gerenciador.proximoLevel);
+        }
+
+        if (GUI.Button(new Rect(Screen.width - 750, 420, 150, 50), "Exit"))
+        {
+            Application.Quit();
+        }
+
+    }
+
+}
---
+++ TankDefense/Assets/Scripts/Menu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public GUISkin skinMenu;
+    public Texture2D Playy;
+    public Texture2D titulo;
+    public Texture2D btnSair;
+
+    private Gerenciador gerenciador;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        gerenciador = FindObjectOfType(typeof(Gerenciador)) as Gerenciador;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = skinMenu;
+        //GUI.DrawTexture(new Rect(Screen.width / 2 - titulo.width / 2, 150,
+        //titulo.width, titulo.height), titulo);
+
+       // bool Playy = GUI.Button("btnStart", true);
+
+        //bool sair = GUI.Button(new Rect(Screen.width / 2 - 93, Screen.height / 2 + 150, 100, 100), btnSair);
+
+
+        if (Playy)
+        {
+            gerenciador.ProximoLevel(gerenciador.proximoLevel);
+        }
+
+        //if (sair)
+        //{
+        //    Application.Quit();
+        //}
+    }
+}
